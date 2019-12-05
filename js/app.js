928db83c0a277e7bb97fad4c357becefcb191143
GayaChoi <woosung827@naver.com>
2019-12-06 00:51:09 +0900

    change

--- js/app.js
+++ js/app.js
@@ -1,17 +1,35 @@
-const btn = $('.btn-confirm');
+const btn = $('.mars-btn');
 
 btn.on('click', function() {
     var url = document.getElementById('url').value;
     var id = url.replace('https://www.youtube.com/watch?v=','');
     var mq = document.getElementById('mq');
-    mq.src = 'http://img.youtube.com/vi/' + id + '/mqdefault.jpg';
-        
+    mq.src = 'https://img.youtube.com/vi/' + id + '/mqdefault.jpg';
+    
+    $('#mq').click(function() {
+        window.open(mq.src);         
+    });    
+
     var hq = document.getElementById('hq');
-    hq.src = 'http://img.youtube.com/vi/' + id + '/hqdefault.jpg';
+    hq.src = 'https://img.youtube.com/vi/' + id + '/hqdefault.jpg';
+
+    $('#hq').click(function() {
+        window.open(hq.src);         
+    });
 
     var sd = document.getElementById('sd');
-    sd.src = 'http://img.youtube.com/vi/' + id + '/sddefault.jpg';
+    sd.src = 'https://img.youtube.com/vi/' + id + '/sddefault.jpg';
+
+    
+    $('#sd').click(function() {
+        window.open(sd.src);         
+    });
 
     var max = document.getElementById('max');
-    max.src = 'http://img.youtube.com/vi/' + id + '/maxresdefault.jpg';
+    max.src = 'https://img.youtube.com/vi/' + id + '/maxresdefault.jpg';
+
+    
+    $('#max').click(function() {
+        window.open(max.src);         
+    });
 });
