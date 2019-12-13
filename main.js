e7732a56325c3be67be1b8b904d7e496985034e2
GayaChoi <woosung827@naver.com>
2019-12-13 20:41:41 +0900

    http

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 var http = require('http');
-var app = require('./app');
-var server = http.createServer(app);
-
-
-server.listen(5300);
+"use strict";
+ 
+require("greenlock-express")
+    .init(function() {
+        var pkg = require("./package.json");
+        return {
+            greenlock: require("@root/greenlock").create({
+                // name & version for ACME client user agent
+                packageAgent: pkg.name + "/" + pkg.version,
+ 
+                // contact for security and critical bug notices
+                maintainerEmail: pkg.author,
+ 
+                // where to find .greenlockrc and set default paths
+                packageRoot: __dirname
+            }),
+ 
+            // whether or not to run at cloudscale
+            cluster: false
+        };
+    })
+    .ready(function(glx) {
+        var app = require("./app");
+ 
+        // Serves on 80 and 443
+        // Get's SSL certificates magically!
+        glx.serveApp(app);
+    });
