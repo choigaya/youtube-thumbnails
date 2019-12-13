0f7f402abc91ca4147277b1f748cde348760b309
GayaChoi <woosung827@naver.com>
2019-12-14 01:30:50 +0900

    green lock test

--- main.js
+++ main.js
@@ -2,32 +2,17 @@ var http = require('http');
 var pkg = require("./package.json");
 
 "use strict";
-
-
 function httpsWorker(glx) {
-    //
-    // HTTPS 1.1 is the default
-    // (HTTP2 would be the default but... https://github.com/expressjs/express/issues/3388)
-    //
+    var app = require("./app");
 
-    // Get the raw https server:
-    var httpsServer = glx.httpsServer(null, function(req, res) {
+    app.get("/hello", function(req, res) {
         res.end("Hello, Encrypted World!");
     });
 
-    httpsServer.listen(443, "0.0.0.0", function() {
-        console.info("Listening on ", httpsServer.address());
-    });
-
-    // Note:
-    // You must ALSO listen on port 80 for ACME HTTP-01 Challenges
-    // (the ACME and http->https middleware are loaded by glx.httpServer)
-    var httpServer = glx.httpServer();
-    httpServer.listen(80, "0.0.0.0", function() {
-        console.info("Listening on ", httpServer.address());
-    });
+    // Serves on 80 and 443
+    // Get's SSL certificates magically!
+    glx.serveApp(app);
 }
-
 
 //require("greenlock-express")
 require("greenlock-express")
@@ -48,4 +33,3 @@ require("greenlock-express")
         };
     })
     .serve(httpsWorker);
-/* server.listen(5300);     */
