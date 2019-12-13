9e238d2f3177e88ed944c6a9df2458f15b0579cd
GayaChoi <woosung827@naver.com>
2019-12-14 01:09:54 +0900

    test

--- main.js
+++ main.js
@@ -1,34 +1,51 @@
 var http = require('http');
-var app = require('./app');
-var server = http.createServer(app);
+var pkg = require("./package.json");
 
 "use strict";
- 
+
+
+function httpsWorker(glx) {
+    //
+    // HTTPS 1.1 is the default
+    // (HTTP2 would be the default but... https://github.com/expressjs/express/issues/3388)
+    //
+
+    // Get the raw https server:
+    var httpsServer = glx.httpsServer(null, function(req, res) {
+        res.end("Hello, Encrypted World!");
+    });
+
+    httpsServer.listen(443, "0.0.0.0", function() {
+        console.info("Listening on ", httpsServer.address());
+    });
+
+    // Note:
+    // You must ALSO listen on port 80 for ACME HTTP-01 Challenges
+    // (the ACME and http->https middleware are loaded by glx.httpServer)
+    var httpServer = glx.httpServer();
+    httpServer.listen(80, "0.0.0.0", function() {
+        console.info("Listening on ", httpServer.address());
+    });
+}
+
+
+//require("greenlock-express")
 require("greenlock-express")
-    .init(function() {
-        var pkg = require("./package.json");
+    .init(function getConfig() {
+        // Greenlock Config
+
         return {
-            greenlock: require("@root/greenlock").create({
-                // name & version for ACME client user agent
-                packageAgent: pkg.name + "/" + pkg.version,
- 
-                // contact for security and critical bug notices
-                maintainerEmail: pkg.author,
- 
-                // where to find .greenlockrc and set default paths
-                packageRoot: __dirname
-            }),
- 
-            // whether or not to run at cloudscale
+            // Package name+version is used for ACME client user agent
+            package: { name: pkg.name, version: pkg.version },
+
+            // Maintainer email is the contact for critical bug and security notices
+            maintainerEmail: "woosung827@naver.com",
+
+            packageRoot: __dirname,
+
+            // Change to true when you're ready to make your app cloud-scale
             cluster: false
         };
     })
-    .ready(function(glx) {
-        var app = require("./app");
- 
-        // Serves on 80 and 443
-        // Get's SSL certificates magically!
-        glx.serveApp(app);
-    });
-
-server.listen(5300);    
+    .serve(httpsWorker);
+/* server.listen(5300);     */
